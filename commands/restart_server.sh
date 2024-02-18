12c3c8f2fe5c68db48765910eb9dd780ae82f569
Sam-Mey <meisongyong@gmail.com>
2024-02-19 07:15:34 +0800

    DockerLNMP

--- commands/restart_server.sh
+++ commands/restart_server.sh
@@ -1,54 +1,14 @@
-# #!/bin/bash
-
-# # Include configuration
-# . /DockerLNMP/config.sh
-
-# # get container_ID
-# database_container_id=$(docker ps -qf "name=mysql" -f "name=mariadb" -f "name=mongodb" -f "name=sqlite")
-
-# nginx_container_id=$(docker ps -qf "name=nginx")
-# php_container_id=$(docker ps -qf "name=php")
-# phpmyadmin_container_id=$(docker ps -qf "name=phpmyadmin")
-# redis_container_id=$(docker ps -qf "name=redis")
-
-# # run restart
-# for container_id in $database_container_id; do
-#     if docker inspect "$container_id" > /dev/null 2>&1; then
-#         docker restart "$container_id"
-#     else
-#         echo "容器 $container_id 不存在或无法访问。"
-#     fi
-# done
-
-# # run restart
-# for container_id in $nginx_container_id $php_container_id $phpmyadmin_container_id $redis_container_id; do
-#     if docker inspect "$container_id" > /dev/null 2>&1; then
-#         docker restart "$container_id"
-#     else
-#         echo "容器 $container_id 不存在或无法访问。"
-#     fi
-# done
-
-# # 这里不能直接打印，代码执行完成后检查每个容器是否重启成功才能
-# echo "所有容器已重启完成。"
-
-
-
 #!/bin/bash
-
-# 设置脚本所在目录为当前工作目录
-cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 # Include configuration
 . /DockerLNMP/config.sh
 
 # get container_ID
-database_container_id=$(docker ps -qf "name=mysql" -f "name=mariadb" -f "name=mongodb" -f "name=sqlite")
-
-nginx_container_id=$(docker ps -qf "name=nginx")
-php_container_id=$(docker ps -qf "name=php")
-phpmyadmin_container_id=$(docker ps -qf "name=phpmyadmin")
-redis_container_id=$(docker ps -qf "name=redis")
+database_container_id=$(docker ps -aqf "name=mysql" -f "name=mariadb" -f "name=mongodb" -f "name=sqlite")
+nginx_container_id=$(docker ps -aqf "name=nginx")
+php_container_id=$(docker ps -aqf "name=php")
+phpmyadmin_container_id=$(docker ps -aqf "name=phpmyadmin")
+redis_container_id=$(docker ps -aqf "name=redis")
 
 # Function to restart container
 restart_container() {
@@ -65,9 +25,10 @@ restart_container() {
 # Restart database containers
 for container_id in $database_container_id; do
     restart_container "$container_id"
-    status=$?
-    if [ $status -eq 0 ]; then
+    if [ $? -eq 0 ]; then
         echo "容器 $container_id 已成功重启。"
+    else
+        echo "容器 $container_id 重启失败。"
     fi
 done
 
@@ -77,10 +38,11 @@ success_count=0
 
 for container_id in "${all_containers[@]}"; do
     restart_container "$container_id"
-    status=$?
-    if [ $status -eq 0 ]; then
+    if [ $? -eq 0 ]; then
         echo "容器 $container_id 已成功重启。"
         ((success_count++))
+    else
+        echo "容器 $container_id 重启失败。"
     fi
 done
 
