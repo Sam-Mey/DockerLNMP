0a7c238b54f260122d936d7b6516a69561c22826
Sam-Mey <meisongyong@gmail.com>
2024-02-19 00:36:53 +0800

    DockerLNMP

--- commands/check_nginx_conf.sh
+++ commands/check_nginx_conf.sh
@@ -5,7 +5,14 @@ nginx_container_id=$(docker ps -qf "name=nginx")
 
 if [ -n "$nginx_container_id" ]; then
     # 进入nginx容器并查找default.conf
-    docker exec -it "$nginx_container_id" find / -name "default.conf" -print -quit
+    nginx_conf_path=$(docker exec -it "$nginx_container_id" find / -name "default.conf" -print -quit)
+    
+    if [ -n "$nginx_conf_path" ]; then
+        # 在nginx容器内使用cat命令打开default.conf
+        docker exec -it "$nginx_container_id" cat "$nginx_conf_path"
+    else
+        echo "default.conf not found in the Nginx container."
+    fi
 else
     echo "Nginx container not found."
 fi
