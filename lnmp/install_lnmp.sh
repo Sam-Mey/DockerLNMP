d8969830e80c5c6b4a5abdd5ef625abb620dc499
Sam-Mey <meisongyong@gmail.com>
2024-02-18 09:48:52 +0800

    DockerLNMP

--- lnmp/install_lnmp.sh
+++ lnmp/install_lnmp.sh
@@ -24,7 +24,7 @@ mv docker docker-phpmyadmin
 find /DockerLNMP -name "*.sh" -exec chmod +x {} \;
 
 chmod -R +x /DockerLNMP
-cd /DockerLNMP/lnmp/www/server
+cd /DockerLNMP/lnmp
 docker-compose -f docker-compose.yml up
 
 echo -e "\e[1;32m环境安装完成，您可以尝试访问公网IP地址：\e[1;34mhttp://IP_域名\e[1;32m 或 \e[1;34mhttps://IP_域名\e[0m"
