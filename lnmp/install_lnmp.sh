2baa306caa5bb403d1655b27c7ba078ba683ef68
Sam-Mey <meisongyong@gmail.com>
2024-02-19 04:50:08 +0800

    DockerLNMP

--- lnmp/install_lnmp.sh
+++ lnmp/install_lnmp.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
+# 设置颜色变量
 BOLD=$(tput bold)
-GRAY=$(tput setaf 0)
 RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-BLUE=$(tput setaf 4)
-PURPLE=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-
 RESET=$(tput sgr0)
 
 # 检查系统是否安装 Docker
@@ -22,6 +15,24 @@ fi
 if ! command -v docker-compose &> /dev/null; then
     echo -e "${RED}未安装 Docker Compose !!! 即将为您安装 Docker Compose...${RESET}"
     # 执行 Docker Compose 安装脚本
+fi
+
+# 提示用户是否安装 compose
+if ! command -v docker-compose &> /dev/null; then
+    read -p "您已安装 Docker，但未安装 Docker Compose。LNMP 环境需要 Docker Compose 支持，是否安装 Docker Compose？ (y/n): " install_compose
+    if [ "$install_compose" == "y" ]; then
+        # 执行 Docker Compose 安装脚本
+    else
+        echo "请先安装 Docker Compose 后再继续创建 LNMP 环境。"
+        exit 1
+    fi
+fi
+
+# 提示用户是否创建 LNMP
+read -p "您已安装了 Docker 与 Docker Compose，符合创建 LNMP 环境的先决条件。是否继续创建 LNMP 环境？ (y/n): " create_lnmp
+if [ "$create_lnmp" != "y" ]; then
+    echo "LNMP 环境创建已取消。"
+    exit 0
 fi
 
 # 定义数据库容器
@@ -40,7 +51,7 @@ for container in "${containers[@]}"; do
 done
 
 # 如果 LNMP 容器不存在，执行创建脚本
-if [ "$lnmp_containers_exist" = false ]; then
+if [ "$lnmp_containers_exist" == false ]; then
     echo "您尚未创建 LNMP 环境，即将为您创建..."
     # 执行 LNMP 创建脚本
 else
