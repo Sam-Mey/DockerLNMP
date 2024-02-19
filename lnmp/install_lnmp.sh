0dad238dc38fd52956ec73dc6dd48d0994e31a6d
Sam-Mey <meisongyong@gmail.com>
2024-02-19 09:41:59 +0800

    DockerLNMP

--- lnmp/install_lnmp.sh
+++ lnmp/install_lnmp.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# 进入脚本所在目录
+cd "$(dirname "$0")"
+
 # Include configuration
 . /DockerLNMP/config.sh
 
+BOLD=$(tput bold)
+GRAY=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+PURPLE=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+
+RESET=$(tput sgr0)
 BOLD=$(tput bold)
 RED=$(tput setaf 1)
 CYAN=$(tput setaf 6)
@@ -10,13 +24,16 @@ RESET=$(tput sgr0)
 
 # 检查系统是否安装 Docker
 if ! command -v docker || ! command -v docker-compose &> /dev/null; then
-    echo -e "${RED}未安装 Docker 现在为您安装...${RESET}"
+    echo -e "${BOLD}${RED} 未安装 Docker 现在为您安装... ${RESET}"
     # 执行 Docker 安装脚本
     "$DOCKER_SCRIPT_PATH"
 else
-    echo -e "${CYAN}您已安装的 Docker 版本：$(docker --version)${RESET}"
-    echo -e "${CYAN}您已安装的 Docker Compose 版本：$(docker-compose --version)${RESET}"
-    echo -e "${RED} 您已安装了docker 与 compose 正在为您创建 LNMP 环境 ${RESET}"
-    # 执行 LNMP 安装脚本
-    "$GET_LNMP_SCRIPT_PATH"
+    echo -e "${GREEN}${CYAN} 您已安装的 Docker 版本：$(docker --version) ${RESET}"
+    echo -e "${GREEN}${CYAN} 您已安装的 Docker Compose 版本：$(docker-compose --version) ${RESET}"
+    echo -e "${GREEN}${RED} 您已安装了docker 与 compose 正在为您创建 LNMP 环境  ${RESET}"
+    # # 执行 LNMP 安装脚本
+    # "$GET_LNMP_SCRIPT_PATH"
 fi
+
+# 执行 LNMP 安装脚本
+"$GET_LNMP_SCRIPT_PATH"
