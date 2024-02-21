fa68060daa1f63af2d7a2536bb8da5a31b0cd525
Sam-Mey <meisongyong@gmail.com>
2024-02-21 12:24:29 +0800

    DockerLNMP

--- acme/acme_centos.sh
+++ acme/acme_centos.sh
@@ -15,7 +15,10 @@ echo_color "green" "请输入您的域名："
 read your_domain
 acme.sh --issue -d "$your_domain" --standalone -k ec-256
 
-# 安装证书到路径
-acme.sh --installcert -d "$your_domain" --ecc --key-file /root/private.key --fullchain-file /root/certificate.crt
-echo_color "green" "证书申请成功，并已安装到 root 目录下；您可以手动更改使用位置！"
+cd root && mkdir certs
 
+# 安装证书到路径
+acme.sh --installcert -d "$your_domain" --ecc --key-file /root/certs/private.key --fullchain-file /root/certs/certificate.crt
+echo_color "green" "证书申请成功，并已安装到 /root/certs 目录下；您可以手动更改使用位置！"
+
+cp certs /DockerLNMP/www/server/nginx
