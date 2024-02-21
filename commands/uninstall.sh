32ba1b482565c9236adbc366dc543007fd11d35b
Sam-Mey <meisongyong@gmail.com>
2024-02-21 15:09:08 +0800

    DockerLNMP

--- commands/uninstall.sh
+++ commands/uninstall.sh
@@ -1,51 +1,32 @@
 #!/bin/bash
 
-# Define colors and styles using tput
-BOLD=$(tput bold)
-GRAY=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-BLUE=$(tput setaf 4)
-PURPLE=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
+# Include configuration
+. /DockerLNMP/config.sh
 
+RED=$(tput setaf 1)  
+GREEN=$(tput setaf 2) 
+YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
-read -p "$(echo -e "${BOLD}${YELLOW}是否确认卸载脚本? (默认为N) | [N/y]: ${RESET}")" -i "N" answer
-if [ "${answer,,}" = "y" ]; then
-    echo -e "${BOLD}${RED} 正在卸载脚本... ${RESET}"
-    # Recursively look for a folder named "DockerLNMP" or "dlnmp" in the root directory
-    result_DockerLNMP=$(find / -type d -name "DockerLNMP" 2>/dev/null)
-    result_dlnmp=$(find / -type d -name "dlnmp" 2>/dev/null)
-    
-    if [ -n "$result_DockerLNMP" ] || [ -n "$result_dlnmp" ]; then
-        # Found 'DockerLNMP' or 'dlnmp' folder(s) at:
-        echo "${BOLD}${RED} 发现 'DockerLNMP' 或 'dlnmp' 文件夹（目录）在：${RESET}"
-        
-        [ -n "$result_DockerLNMP" ] && echo "${BOLD}${RED} $result_DockerLNMP ${RESET}"
-        [ -n "$result_dlnmp" ] && echo "${BOLD}${RED} $result_dlnmp ${RESET}"
-        
-        # confirm deletion ?
-        read -p "${BOLD}${YELLOW}是否要删除这些文件夹？ (Y/N): ${RESET}" confirmation
-        if [ "$confirmation" == "Y" ] || [ "$confirmation" == "y" ]; then
-            # Delete 'DockerLNMP' and 'dlnmp' folders?
-            [ -n "$result_DockerLNMP" ] && rm -rf "$result_DockerLNMP"
-            [ -n "$result_dlnmp" ] && rm -rf "$result_dlnmp"
-            
-            # Remove '/usr/local/bin/dlnmp' link
-            rm -f /usr/local/bin/dlnmp
-            
-            echo "${BOLD}${RED} 删除了 'DockerLNMP' 主体 和 'dlnmp' 快捷方式. ${RESET}"
-        else
-            echo "${BOLD}${GREEN} 取消了删除操作 ${RESET}"
-        fi
+echo "${RED}此脚本将删除所有匹配 DockerLNMP 目录与 dlnmp 或文件。请谨慎使用。${RESET}"
+
+# 查找匹配的目录或文件
+matches=$(sudo find / -type d -name "DockerLNMP" -o -type d -name "dlnmp" -o -type f -name "DockerLNMP" -o -type f -name "dlnmp")
+
+if [ -n "$matches" ]; then
+    echo echo -e "${BOLD}${YELLOW} 找到以下匹配项：${RESET}"
+    echo echo -e "${RED}$matches${RESET}"
+
+    read -p "${BOLD}${YELLOW} 是否执行删除？ (y/n): ${RESET}" choice
+
+    if [ "$choice" == "y" ]; then
+        # 执行删除命令
+        sudo rm -rf $matches
+        echo -e "${BOLD}${GREEN} 删除完成。${RESET}"
     else
-        echo "${BOLD}${RED} 在任何目录中找不到关于 'DockerLNMP' 或 'dlnmp' 文件夹 ${RESET}"
+        echo -e "${BOLD}${RED} 取消删除。${RESET}"
     fi
-    
-    exit 0
 else
-    echo -e "${BOLD}${YELLOW} 脚本卸载已取 ${RESET}"
+    echo -e "${BOLD}${YELLOW} 未找到有关 DockerLNMP 与 dlnmp 文件。${RESET}"
 fi
+
