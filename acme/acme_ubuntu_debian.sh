5c278850585bcc216fa8c8301a914e71397e16e2
Sam-Mey <meisongyong@gmail.com>
2024-02-23 14:46:14 +0800

    DockerLNMP

--- acme/acme_ubuntu_debian.sh
+++ acme/acme_ubuntu_debian.sh
@@ -1,17 +1,21 @@
-#!/bash/bin
+#!/bin/bash
 
-echo_color "green" "正在安装必要的依赖和获取证书..."
+BOLD=$(tput bold) 
+GREEN=$(tput setaf 2) 
+RESET=$(tput sgr0)
+
+echo -e "${BOLD}${GREEN}正在安装必要的依赖和获取证书...${RESET}"
 apt-get install -y socat
 curl https://get.acme.sh | sh
 ln -s /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
 
 # 用户注册
-echo_color "green" "请输入您的邮箱地址："
+echo -e "${BOLD}${GREEN}请输入您的邮箱地址：${RESET}"
 read email_address
 acme.sh --register-account -m "$email_address"
 
 # 开始申请证书
-echo_color "green" "请输入您的域名："
+echo -e "${BOLD}${GREEN}请输入您的域名：${RESET}"
 read your_domain
 acme.sh --issue -d "$your_domain" --standalone -k ec-256
 
@@ -19,6 +23,6 @@ cd root && mkdir certs
 
 # 安装证书到路径（默认 root 目录）
 acme.sh --installcert -d "$your_domain" --ecc --key-file /root/certs/private.key --fullchain-file /root/certs/certificate.crt
-echo_color "green" "证书申请成功，并已安装到 root/certs 目录下；您可以手动更改使用位置！"
+echo -e "${BOLD}${GREEN}证书申请成功，并已安装到 root/certs 目录下；您可以手动更改使用位置！${RESET}"
 
-cp certs /DockerLNMP/www/server/nginx
+cp -r certs /DockerLNMP/www/server/nginx
