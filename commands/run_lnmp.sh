a5967637955c6ff1925d5b804321a2cc9b06376d
Sam-Mey <meisongyong@gmail.com>
2024-02-23 10:56:21 +0800

    DockerLNMP

--- commands/run_lnmp.sh
+++ commands/run_lnmp.sh
@@ -4,31 +4,15 @@
 cd /
 git clone https://github.com/Sam-Mey/DockerLNMP.git
 
-# Copy certs directory to nginx server
-# cp -r root/certs /DockerLNMP/www/server/nginx
+# Change directory to the DockerLNMP folder
+cd /DockerLNMP/lnmp
 
-# Change directory to the  folder
-# cd /DockerLNMP/lnmp
-
-# Clone necessary repositories
-cd /DockerLNMP/lnmp/nginx \
-    git clone https://github.com/nginxinc/docker-nginx.git
-
-cd /DockerLNMP/lnmp/mariadb \
-    git clone https://github.com/MariaDB/mariadb-docker.git \
-    mv mariadb-docker docker-mariadb
-
-cd /DockerLNMP/lnmp/php \
-    git clone https://github.com/docker-library/php.git \
-    mv php docker-php
-
-cd /DockerLNMP/lnmp/phpmyadmin \
-    git clone https://github.com/phpmyadmin/docker.git \
-    mv docker docker-phpmyadmin
-
-cd /DockerLNMP/lnmp/redis \
-    git clone https://github.com/docker-library/redis.git \
-    mv redis docker-redis
+# Clone necessary repositories and move them
+cd nginx && git clone https://github.com/nginxinc/docker-nginx.git && cd ..
+cd mariadb && git clone https://github.com/MariaDB/mariadb-docker.git && mv mariadb-docker docker-mariadb && cd ..
+cd php && git clone https://github.com/docker-library/php.git && mv php docker-php && cd ..
+cd phpmyadmin && git clone https://github.com/phpmyadmin/docker.git && mv docker docker-phpmyadmin && cd ..
+cd redis && git clone https://github.com/docker-library/redis.git && mv redis docker-redis && cd ..
 
 # Set execute permissions for shell scripts
 find /DockerLNMP -name "*.sh" -exec chmod +x {} \;
@@ -41,7 +25,6 @@ cd /DockerLNMP/lnmp
 
 # Bring up the Docker Compose environment
 docker-compose -f docker-compose.yml up
-# docker stack deploy -c docker-compose.yml lnmp-stack
 
 # Print completion message
 echo -e "\e[1;32m环境安装完成，您可以尝试访问公网IP地址：\e[1;34mhttp://IP_域名\e[1;32m 或 \e[1;34mhttps://IP_域名\e[0m"
