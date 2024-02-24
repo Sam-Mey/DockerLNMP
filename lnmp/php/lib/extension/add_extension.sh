950235a2523b28975b3089988408035dc677f788
Sam-Mey <meisongyong@gmail.com>
2024-02-24 20:08:24 +0800

    DockerLNMP

--- lnmp/php/lib/extension/add_extension.sh
+++ lnmp/php/lib/extension/add_extension.sh
@@ -19,22 +19,21 @@
 
 
 #==============Alpine Extension==================
-apk add php82-bcmath
-apk add php82-bz2
-apk add php82-cli
-apk add php82-common
-apk add php82-curl
-apk add php82-fpm
-apk add php82-gd
-apk add php82-mbstring
-apk add php82-opcache
-apk add php82-redis
-apk add php82-xml
-apk add php82-zip2
-
-apk add php82-igbinary # no, You need to clone the GitHub source code to compile and install
-apk add php82-mysql # no, you need replace php83-mysqli
-apk add php82-readline # no, You need to clone the GitHub source code to compile and install
-apk add php82-yaml # no, You need to clone the GitHub source code to compile and install
+apt install php8.2-bcmath
+apt install php8.2-bz2
+apt install php8.2-cli
+apt install php8.2-common
+apt install php8.2-curl
+apt install php8.2-fpm
+apt install php8.2-gd
+apt install php8.2-igbinary
+apt install php8.2-mbstring
+apt install php8.2-mysql
+apt install php8.2-opcache
+apt install php8.2-readline
+apt install php8.2-redis
+apt install php8.2-xml
+apt install php8.2-yaml
+apt install php8.2-zip
 #==============Alpine Extension==================
 
